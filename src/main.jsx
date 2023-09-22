97adae5b42c988d980cff6922fe7be8abf0d91ae
FxShadow <sebastian.vasquezparra@inemjose.edu.co>
2023-09-22 08:03:58 -0500

    p4

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import router from './config/router'
-import { store } from './context/Store'
+import { store } from './context/store'
 
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
